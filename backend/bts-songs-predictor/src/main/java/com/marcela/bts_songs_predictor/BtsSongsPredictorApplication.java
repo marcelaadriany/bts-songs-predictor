2e88422cc40a33a2067b2c27137bc88769420daa
marcelaadriany <marcela.adriany@gmail.com>
2026-04-28 15:59:25 -0300

    chore: initial commit

---
+++ backend/bts-songs-predictor/src/main/java/com/marcela/bts_songs_predictor/BtsSongsPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.marcela.bts_songs_predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtsSongsPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtsSongsPredictorApplication.class, args);
+	}
+
+}
---
+++ backend/bts-songs-predictor/src/test/java/com/marcela/bts_songs_predictor/BtsSongsPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcela.bts_songs_predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtsSongsPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
